3955e18575d784d08a4c92be1a5c9b1be5fa412e
Hugo0X <hugo95martin@gmail.com>
2019-11-18 15:50:15 +0100

    add comment

--- controleur/c_deconnexion.php
+++ controleur/c_deconnexion.php
@@ -1,6 +1,6 @@
 <?php
     // supprimer la session
     session_destroy();
-    // redirection vers le controleur principal !!!!! pas de HTML avant !!!!!
+    // redirection vers le controleur principal !!!!! pas de HTML avant !!!!! gg
     header('Location: index.php');
 ?>
