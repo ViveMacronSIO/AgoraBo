3a883c0cbe8d5bc51e1b90417989943e4e2ddce4
Benjamin <benjaminkrause57@gmail.com>
2019-11-27 11:43:54 +0100

    changement base de données du au renommage

--- app/_config.inc.php
+++ app/_config.inc.php
@@ -13,13 +13,16 @@ ini_set('error_reporting', E_ALL);  	// en phase de développement
 
 // constantes pour l'accès à la base de données
 define('DB_SERVER', 'localhost');	// serveur MySql
-define('DB_DATABASE', 'jeux');		// nom de la base de données
+define('DB_DATABASE', 'agora');		// nom de la base de données
 define('DB_USER', 'userAgoraBo');			// nom d'utilisateur
 define('DB_PWD', 'wWlM2VEtnKQBI9md');              	// mot de passe
 define('DSN','mysql:dbname='.DB_DATABASE.';host='.DB_SERVER);
-
+// nom d'utilisateur et mot de passe pour se connecter au site sur wampserver dubois  azer1
 // constantes pour twig
 define('TWIG_CACHE', false);          // mise en cache, en production à remplacer par  '/path/to/compilation_cache'
 define('TWIG_DEBUG', true);           // mode debug
 
+
 ?>
+
+
